266e3109a60750d95da6075ed704489f73016fd6
Radu <raduzamfir1199@yahoo.com>
2021-09-28 16:43:59 +0300

    commit

---
+++ src/apollo/defaultCacheData.js
@@ -0,0 +1,11 @@
+// import { emptyString } from 'utils/constants'
+import { emailKey } from './cacheKeyFunctions'
+
+// Here you define the default values for local apollo state (@client only values)
+// https://www.apollographql.com/docs/react/local-state/local-state-management/
+
+const defaultEmail = { email: 'abc@gmail.com' }
+
+export const defaults = {
+  [emailKey]: defaultEmail
+}
---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,39 @@
+/* eslint-disable react/jsx-no-bind */
+import React from 'react'
+import { Switch, Redirect } from 'react-router-dom'
+
+import CustomRoute from '../components/routing/CustomRoute'
+
+import Welcome from 'features/welcome/Welcome'
+import Settings from 'features/settings/Settings'
+import { Forbidden, NotFound } from '@bit/totalsoft_oss.react-mui.kit.core'
+import HelloWorld from 'features/helloWorld/HelloWorld'
+import { useEmail } from 'hooks/useEmail'
+import ConferenceListContainer from 'features/conferences/components/ConferenceListContainer'
+import MyConferenceListContainer from 'features/myConference/list/components/MyConferenceListContainer'
+
+export default function AppRoutes() {
+  const [email] = useEmail()
+
+  if (!email) {
+    return (
+      <Switch>
+        <CustomRoute isPrivate={false} exact path='/welcome' component={Welcome} />
+        <Redirect to='/welcome' />
+      </Switch>
+    )
+  }
+
+  return (
+    <Switch>
+      <CustomRoute isPrivate={false} exact path='/helloWorld' component={HelloWorld} />
+      <CustomRoute isPrivate={false} exact path='/welcome' component={Welcome} />
+      <CustomRoute isPrivate={false} exact path='/conferences' component={ConferenceListContainer} />
+      <CustomRoute isPrivate={false} exact path='/myConferences' component={MyConferenceListContainer} />
+      <CustomRoute exact path='/settings' component={Settings} />
+      <Redirect exact from='/' to='/welcome' />
+      <CustomRoute isPrivate={false} exact path='/forbidden' component={Forbidden} />
+      <CustomRoute isPrivate={false} render={() => <NotFound title='PageNotFound'></NotFound>} />
+    </Switch>
+  )
+}
